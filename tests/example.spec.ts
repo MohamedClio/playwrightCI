76ccf1083378dbd14128f697f9b709a1af24ee7e
@herrmclio <herrmclio@gmail.com>
2024-06-07 17:46:30 +0200

    completing test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("Has navbar links", async ({ page }) => {
   await page.goto("https://www.oska-health.com/");
+  await page.getByTestId("uc-accept-all-button").click();
 
-  await page.getByRole("link", { name: "Patient:innen" }).click();
+  await page
+    .locator("header")
+    .getByRole("link", { name: "Patient:innen" })
+    .click();
+  await expect(
+    page.getByRole("heading", { name: "Das bietet Ihnen Oska" })
+  ).toBeVisible();
 });
